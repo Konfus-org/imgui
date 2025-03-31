0a87041ffe230e661d5c707f9d6dd1b8c5ac5d0a
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-03-31 15:24:11 -0700

    Adding premake file

---
+++ premake5.lua
@@ -0,0 +1,46 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "Off"
+
+    if OutputIntermediateDir == nil or OutputTargetDir == nil then
+        targetdir ("Build/bin/%{prj.name}/")
+        objdir    ("Build/obj/%{prj.name}/")
+
+    else
+        targetdir ("../../../" .. OutputTargetDir .. "")
+        objdir    ("../../../" .. OutputIntermediateDir .. "")
+    end
+
+    files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+    includedirs
+    {
+        "./"
+    }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "on"
+        symbols "off"
