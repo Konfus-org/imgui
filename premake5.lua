d182251341557162aba8929b06328b1266501890
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-04-07 18:22:58 -0700

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -20,16 +20,34 @@ project "ImGui"
 		"imgui.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
+		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+
+        -- Need to swap out backends depending on platform
+        -- Default is GLFW and OpenGL
+        "./backends/imgui_impl_glfw.h",
+        "./backends/imgui_impl_glfw.cpp",
+        "./backends/imgui_impl_opengl3.h",
+        "./backends/imgui_impl_opengl3.cpp",
 	}
 
     includedirs
     {
-        "./"
+        "./",
+        "./backends",
+
+        -- Default to GLFW backend
+        "%{IncludeDir.GLFW}"
+    }
+
+    links
+    {
+        -- Default to GLFW backend
+        "GLFW"
     }
 
     filter "configurations:Debug"
