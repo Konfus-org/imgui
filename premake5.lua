b0ac036c6177c32479e5e5cd4a026427009feba5
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-04-08 17:29:22 -0700

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -52,13 +52,16 @@ project "ImGui"
 
     filter "configurations:Debug"
         runtime "Debug"
+        linkoptions { "/LTCG:incremental" }  -- Enable Link Time Code Generation and Incremental linking
         symbols "on"
+
+    filter "configurations:Optimized"
+        runtime "Release"
+        linkoptions { "/LTCG:incremental" }  -- Enable Link Time Code Generation and Incremental linking
+        optimize "on"
 
     filter "configurations:Release"
         runtime "Release"
         optimize "on"
-
-    filter "configurations:Dist"
-        runtime "Release"
-        optimize "on"
+        linkoptions { "/LTCG:incremental" }  -- Enable Link Time Code Generation and Incremental linking
         symbols "off"
